a24e0b5a225ae7f7e4c4462929d6c606e5602a0d
Mario Andrés Fernández <57473369+mafernga@users.noreply.github.com>
2021-01-12 17:01:26 -0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//importaciones que nosotros escribimos
+//import 'src/app.dart';
+import 'package:contador/src/app.dart';
+
+void main() {
+  runApp(new MyApp());
+}
